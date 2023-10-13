d9878f4f6232b04a34f1a1cb49faaa369e499629
MMurtazaKhan <murtaza10dec@gmail.com>
2023-10-13 23:22:11 +0500

    port

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(notFound)
 app.use(errorHandler)
 
 // Start the server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 connectDB().then(() => {
     app.listen(port, () => {
         console.log(`server is running on port ${port}`);
