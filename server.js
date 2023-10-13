7d5f748118bb59ea29aca1b78e784a3e7fee74d3
MMurtazaKhan <murtaza10dec@gmail.com>
2023-10-13 23:03:53 +0500

    chnages

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(notFound)
 app.use(errorHandler)
 
 // Start the server
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 connectDB().then(() => {
     app.listen(port, () => {
         console.log("listening for requests");
